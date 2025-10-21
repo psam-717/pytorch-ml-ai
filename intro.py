5b6f65a9c15b22bfb3597b6d79fcfdee7ca1843a
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-21 22:49:13 +0000

    Implement tensor initialization with sample data, ones_like, and random tensors, including print statements for verification

---
+++ intro.py
@@ -0,0 +1,10 @@
+import torch
+# import torchvision
+
+data = [[1,2], [3,4]]
+x_data = torch.tensor([data])
+x_ones = torch.ones_like(x_data)
+x_random = torch.rand(4,3)
+print(f"Tensor data: \n {x_data} \n")
+print(f"Ones tensor \n {x_ones} \n")
+print(f"Random tensor \n {x_random} \n")
