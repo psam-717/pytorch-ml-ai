7d5113d4c214166f02e6e53a8c5ca2652da66546
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-21 22:48:40 +0000

    Implement vector-valued function Q with cubic and quadratic terms, compute gradients using PyTorch autograd

---
+++ autograd_diff.py
@@ -0,0 +1,15 @@
+import torch 
+
+# creating tensors and tracking operations for gradient computation
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+
+# define a vector valued function with vectors 'a' and 'b'
+Q = 3*a**3 - b**2
+
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad) # perform differentiation
+
+# check if collected gradient are correct
+print(9*a**2 == a.grad)
+print(-2*b == b.grad)
