3776c916c0762a57cce32ef10a57568a13c638d1
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-12-01 23:45:17 +0000

    no major change

--- intro.py
+++ intro.py
@@ -1,6 +1,7 @@
 import torch
 # import torchvision
 
+# no major change
 data = [[1,2], [3,4]]
 x_data = torch.tensor([data])
 x_ones = torch.ones_like(x_data)
@@ -8,3 +9,4 @@ x_random = torch.rand(4,3)
 print(f"Tensor data: \n {x_data} \n")
 print(f"Ones tensor \n {x_ones} \n")
 print(f"Random tensor \n {x_random} \n")
+ 
